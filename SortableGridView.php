fb1ce7463f8ee1567dab2f7ccbd9ac4979bd2a8b
Denis Amelchenko <denis.dever@gmail.com>
2015-11-17 21:11:13 +0600

    Update SortableGridView.php

--- SortableGridView.php
+++ SortableGridView.php
@@ -1,21 +1,10 @@
 <?php
-/**
- * @link https://github.com/himiklab/yii2-sortable-grid-view-widget
- * @copyright Copyright (c) 2014 HimikLab
- * @license http://opensource.org/licenses/MIT MIT
- */
 
-namespace himiklab\sortablegrid;
+namespace twixoff\sortablegrid;
 
 use yii\helpers\Url;
 use yii\grid\GridView;
 
-/**
- * Sortable version of Yii2 GridView widget.
- *
- * @author HimikLab
- * @package himiklab\sortablegrid
- */
 class SortableGridView extends GridView
 {
     /** @var string|array Sort action */
