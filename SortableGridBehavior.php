4e3f419da917db8a7d61a00c963b799593c66e31
himiklab <himiklab@users.noreply.github.com>
2018-02-27 16:58:01 +0300

    unknown column if alias set in ActiveRecord::find method (issue #30)

--- SortableGridBehavior.php
+++ SortableGridBehavior.php
@@ -89,6 +89,9 @@ class SortableGridBehavior extends Behavior
             call_user_func($this->scope, $query);
         }
 
+        /* Override model alias if defined in the model's class */
+        $query->from([$model::tableName() => $model::tableName()]);
+
         $maxOrder = $query->max('{{' . trim($model::tableName(), '{}') . '}}.[[' . $this->sortableAttribute . ']]');
         $model->{$this->sortableAttribute} = $maxOrder + 1;
     }
